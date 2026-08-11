f2dbdc56cdfd1974d4ca617a6bd55d7ededdfcf8
Mateo Rial <mateo.rial@gmail.com>
2026-08-11 10:48:10 -0300

    working for claudflare

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { Dashboard } from "./components/Dashboard";
 import { getLotes } from "@/lib/data";
 
-export const revalidate = 0; // Disable static prerendering
+export const revalidate = 0; // dDisable static prerendering
 
 export default async function Page() {
   const lotes = await getLotes();
