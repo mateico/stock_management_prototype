fac7bcb05794c5a7e0d8e9ebb5c3e63773ed2c3c
Mateo Rial <mateo.rial@gmail.com>
2026-08-10 22:33:39 -0300

    disable static prerendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { Dashboard } from "./components/Dashboard";
 import { getLotes } from "@/lib/data";
 
+export const revalidate = 0; // Disable static prerendering
+
 export default async function Page() {
   const lotes = await getLotes();
 
